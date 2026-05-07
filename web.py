e33c276ae145fce3d4a62f279390afc71f136527
chang200601-ubs <chang200601@gmail.com>
2026-05-07 11:51:41 +0800

    0507v.3

--- web.py
+++ web.py
@@ -42,14 +42,84 @@ def index():
     link += "<a href=/moviesearch>爬取即將上映的電影</a><hr>"
     link += "<a href=/spidermovie>讀取開眼電影即將上映影片，寫入Firestore</a><br><hr>"
     link += "<a href=/readmovie> 輸入片名關鍵字,查詢資料庫符合的電影包含編號,片名,海報,介紹頁及上映日期</a><br><hr>"
-    link += "<a href=/rode>台中市十大肇事路口</a><br><hr>"
+    link += "<a href=/road>台中市十大肇事路口</a><br><hr>"
+    link += "<a href=/weather>天氣</a><br><hr>"    
     return link
 
 
+@app.route("/weather")
+def weather():
+    city = request.args.get("city", "")  # 從網址參數取得縣市
+    
+    # 修正重點：先將 R 初始化為空字串
+    R = "<h1>天氣查詢</h1>" 
+    
+    API_KEY = "rdec-key-123-45678-011121314"
+    
+    # 搜尋表單
+    R += """
+    <form action="/weather" method="get">
+        <select name="city">
+            <option value="">請選擇縣市…</option>
+            <option>臺北市</option><option>新北市</option><option>桃園市</option>
+            <option>臺中市</option><option>臺南市</option><option>高雄市</option>
+            <option>基隆市</option><option>新竹市</option><option>新竹縣</option>
+            <option>苗栗縣</option><option>彰化縣</option><option>南投縣</option>
+            <option>雲林縣</option><option>嘉義市</option><option>嘉義縣</option>
+            <option>屏東縣</option><option>宜蘭縣</option><option>花蓮縣</option>
+            <option>臺東縣</option><option>澎湖縣</option><option>金門縣</option>
+            <option>連江縣</option>
+        </select>
+        <input type="submit" value="搜尋">
+    </form><hr>
+    """
+    
+    if city:
+        try:
+            url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorization={API_KEY}&format=JSON&locationName={city}"
+            Data = requests.get(url, verify=False)
+            JsonData = json.loads(Data.text)
+            
+            if not JsonData["records"]["location"]:
+                R += "<p class='error'>找不到該地區資料</p>"
+            else:
+                loc = JsonData["records"]["location"][0]
+                
+                # 取得天氣元素
+                elements = {e["elementName"]: e for e in loc["weatherElement"]}
+                wx = elements.get("Wx")
+                pop = elements.get("PoP")
+                minT = elements.get("MinT")
+                maxT = elements.get("MaxT")
+                
+                labels = ["今日白天", "今夜至明晨", "明日白天"]
+                R += f"<h2>{loc['locationName']} 天氣預報</h2>"
+                
+                for i in range(len(wx["time"])):
+                    desc  = wx["time"][i]["parameter"]["parameterName"]
+                    rain  = pop["time"][i]["parameter"]["parameterName"]
+                    lo    = minT["time"][i]["parameter"]["parameterName"]
+                    hi    = maxT["time"][i]["parameter"]["parameterName"]
+                    start = wx["time"][i]["startTime"][5:16].replace("T", " ")
+                    end   = wx["time"][i]["endTime"][5:16].replace("T", " ")
+                    
+                    R += f"""
+                    <div style="border:1px solid #ccc; padding:12px; margin:8px 0; border-radius:6px">
+                        <b>{labels[i]}</b> ({start} ~ {end})<br>
+                        天氣：{desc}<br>
+                        溫度：{lo}°C ～ {hi}°C<br>
+                        降雨機率：{rain}%
+                    </div>"""
+                    
+        except Exception as e:
+            R += f"<p style='color:red'>查詢失敗：{str(e)}</p>"
+    
+    R += "<br><a href='/'>返回首頁</a>"
+    return R
 
-@app.route("/rode")
-def rode():
-    R = "<h1>台中市十大肇事路口(113年10月)</h1><br>" 
+@app.route("/road")
+def road():
+    R = "<h1>台中市十大肇事路口(113年10月)By 張伊傑</h1><br>" 
     url = "https://datacenter.taichung.gov.tw/swagger/OpenData/a1b899c0-511f-4e3d-b22b-814982a97e41"
     headers={'User-Agent':'Mozilla/5.0'}
     Data = requests.get(url,verify=False)
