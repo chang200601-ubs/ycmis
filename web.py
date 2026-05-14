a203d85e1ec0d765e489d911f28e0eff1f3b2f31
chang200601-ubs <chang200601@gmail.com>
2026-05-14 11:43:30 +0800

    webhook5

--- web.py
+++ web.py
@@ -54,14 +54,23 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req["queryResult"]["action"]
-    #msg =  req["queryResult"]["queryText"]
-    #info = "我是張伊傑做的機器人,動作：" + action + "； 查詢內容：" + msg
-
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
     if (action == "rateChoice"):
         rate =  req["queryResult"]["parameters"]["rate"]
-        info = "我是張伊傑做的機器人,您選擇的電影分級是：" + rate
-
+        info = "我是張伊傑開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
+        db = firestore.client()
+        collection_ref = db.collection("電影含分級")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                result += "介紹：" + dict["hyperlink"] + "\n\n"
+        info += result
     return make_response(jsonify({"fulfillmentText": info}))
+
 
 @app.route("/rate")
 def rate():
