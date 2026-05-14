7f4e999dc5ed108e227fb837b5c591dbce333b50
chang200601-ubs <chang200601@gmail.com>
2026-05-14 11:02:51 +0800

    webhook1

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import os
 import json
 import firebase_admin
 from firebase_admin import credentials, firestore
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,make_response, jsonify
 from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
@@ -46,6 +46,17 @@ def index():
     link += "<a href=/weather>天氣查詢</a><br><hr>"    
     link +="<a href=/rate>本周新片進DB</a><br><hr>" 
     return link
+
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
 
 @app.route("/rate")
 def rate():
