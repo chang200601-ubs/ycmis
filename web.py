4bab3db0ed1ba91ec508e19a6c39e416c6b02f80
chang200601-ubs <chang200601@gmail.com>
2026-05-28 10:44:32 +0800

    AI-1

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 import os
 import json
 import firebase_admin
+from google import genai
 from firebase_admin import credentials, firestore
 from flask import Flask, render_template, request,make_response, jsonify
 from datetime import datetime
@@ -24,6 +25,9 @@ db = firestore.client()
 
 # 2. Flask 初始化
 app = Flask(__name__)
+
+client = genai.Client()
+
 
 @app.route("/")
 def index():
@@ -75,6 +79,17 @@ def webhook():
         info += result
     return make_response(jsonify({"fulfillmentText": info}))
 
+
+@app.route("/AI")
+def AI():
+    # 每次使用者拜訪該路徑時，直接使用全域的 client 呼叫模型
+    response = client.models.generate_content(
+        model='gemini-3.5-flash',
+        contents='我想查詢靜宜大學資管系的評價？',
+    )
+    
+    # 回傳生成的文字
+    return response.text
 
 @app.route("/rate")
 def rate():
